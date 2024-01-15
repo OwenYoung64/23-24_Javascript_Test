795f7344d99d21a6a8fe4b5e5fcef7a3450a8126
OwenYoung64 <90921938+OwenYoung64@users.noreply.github.com>
2024-01-15 22:23:30 +0000

    third part done

--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -1,1 +1,6 @@
 /* Put code for the task below */
+/* make a function that changes the text in basic directions when clicked */
+function changeText(){
+  const paragraph = document.getElementById("paragraph")
+  paragraph.innerHTML = "I Clicked It!"
+}
