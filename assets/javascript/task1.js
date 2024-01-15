d1bc9629cc253b2535399788de5ac3b1e45c9142
OwenYoung64 <90921938+OwenYoung64@users.noreply.github.com>
2024-01-15 20:41:43 +0000

    first done

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,14 @@
 /* Put code for the task below */
+/* Create function for button display */
+
+function imagehover() {
+  const button1 = document.getElementById("buttonTask1")
+  button1.style.display = "block"
+}
+imageTask1.addEventListener(onmouseover)
+
+/* look for button click */
+function buttonclick() {
+  const image1 = document.getElementById("imageTask1")
+  image1.style.display = "none"
+}
