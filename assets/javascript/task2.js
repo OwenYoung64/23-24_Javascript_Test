a80a0eaa557df73de585fc21e019415cc937ea61
OwenYoung64 <90921938+OwenYoung64@users.noreply.github.com>
2024-01-15 20:54:46 +0000

    part 2 finished

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,18 @@
 /* Put code for the task below */
+/* code for changing the color of the table to red using button1 */
+function button2(){
+  const table1 = document.getElementById("table1")
+  table1.style.backgroundColor = "red"
+}
+
+/* code for changing the color of the table to green using button2 */
+function button3(){
+  const table1 = document.getElementById("table1")
+  table1.style.backgroundColor = "green"
+}
+
+/* code for changing the color of the table to blue using button3 */
+function button4(){
+  const table1 = document.getElementById("table1")
+  table1.style.backgroundColor = "blue"
+}
