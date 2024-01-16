bdfbe6e04db48a373548d2f5627eb363700b9758
OwenYoung64 <90921938+OwenYoung64@users.noreply.github.com>
2024-01-16 03:04:10 +0000

    almost done

--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -1,1 +1,37 @@
 /* Put code for the task below */
+/* constants */
+const submit = document.getElementById('submit');
+const name = document.getElementById('name');
+const address = document.getElementById('address');
+const phone = document.getElementById('phone');
+const email = document.getElementById('email');
+
+/* event listener */
+submit.addEventListener('click', formCheck);
+
+/* function to check if the form is empty or filled when the submit button is pressed */
+function formCheck() {
+  if (name.value === '') {
+    name.style.backgroundColor = "red";
+  } else {
+    name.style.backgroundColor = 'white';
+  }
+
+  if (address.value === '') {
+    address.style.backgroundColor = "red";
+  } else {
+    address.style.backgroundColor = 'white';
+  }
+
+  if (phone.value === '') {
+    phone.style.backgroundColor = "red";
+  } else {
+    phone.style.backgroundColor = 'white';
+  }
+
+  if (email.value === '') {
+    email.style.backgroundColor = "red";
+  } else {
+    email.style.backgroundColor = 'white';
+  }
+}
