0631bf72e6d7129894d01a75850c44f5d5157e1d
OwenYoung64 <90921938+OwenYoung64@users.noreply.github.com>
2024-01-16 00:29:23 +0000

    forth part done

--- assets/javascript/task4.js
+++ assets/javascript/task4.js
@@ -1,1 +1,9 @@
 /* Put code for the task below */
+/* function that when the button is clicked, opens a new window */
+function clearWindow() {
+  /* create a new blank window  - create a var and assign it to open a window */
+  var windowopen = window.open("");
+  
+  /* make the new page have text - use the variable to write in the new document the required text */
+  windowopen.document.writeln("new page has been created")
+}
